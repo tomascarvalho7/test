2de40ec24fbe20480b0cc5525c4867fe274095a9
tomascarvalho7 <tomascarvalho7@gmail.com>
2024-11-01 15:39:56 +0000

    Removed apt update and python install from run script

--- ner/setup_and_run.sh
+++ ner/setup_and_run.sh
@@ -2,9 +2,6 @@
 
 python3 -m venv myenv
 source myenv/bin/activate
-sudo apt update && sudo apt install python3.x
-
-pip install --upgrade pip
 pip install -r requirements.txt
 
 bash ./scripts/run_token_ner.sh
